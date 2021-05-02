f53e9c8e5234f5eab3a91cb2c82d9bbed53de41b
hyeonukdev <lego7407@gmail.com>
2021-05-02 21:53:12 +0900

    twoSum

--- arrangement/TwoSum.py
+++ arrangement/TwoSum.py
@@ -15,3 +15,24 @@ def twoSum(nums, target):
 nums = [2, 7, 11, 15]
 target =9
 print(twoSum(nums, target))
+
+# in 탐색
+def twoSum_1(nums, target):
+    for i, n in enumerate(nums):
+        complement = target - n
+
+        if complement in nums[i +1:]:
+            return nums.index(n), nums[i +1:].index(complement)+ (i+1)
+
+print(twoSum_1(nums, target))
+
+
+# 조회 구조 개선
+def twoSum_2(nums, target):
+    nums_map = {}
+    for i, num in enumerate(nums):
+        if target - num in nums_map:
+            return [nums_map[target - num], i]
+        nums_map[num] = i
+
+print(twoSum_2(nums, target))
